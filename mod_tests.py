3a99736b891bb2a1dd318adbaeb8e878645d19ac
mfaggetter <mfaggetter@gmail.com>
2016-06-30 15:45:28 +0100

    started local repositry

---
+++ mod_tests.py
@@ -0,0 +1,10 @@
+# This is for you to fill in!
+
+from mean import mean
+
+def test_mean_1234():
+    num_list  = [1,2,3,4,5]
+    calc_mean = mean(num_list)
+    expected_mean = 3
+    assert expected_mean == calc_mean
+
